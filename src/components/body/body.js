a6e8ebabd02f10ad6efa9a8ae192cf07b5cb708a
millionsime <simemillion@gmail.com>
2023-10-11 10:52:29 +0300

    yamemeg let tewat

---
+++ src/components/body/body.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './body.css'
+
+export default function body() {
+  return (
+    <div className='body-div'>
+      <div className="App">
+      <h2>hey there!</h2>
+      <h2>hey there!</h2>
+      <h2>hey there!</h2>
+      <h2>hey there!</h2>
+      <h2>hey there!</h2>
+      <h2>hey there!</h2>
+    </div>
+    </div>
+  )
+}
